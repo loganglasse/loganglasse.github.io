39eafa7d23ee86a97a4a9c563cfbf85476a0e911
Logan Glasse on School Computer <loganglasse90@gmail.com>
2019-10-28 12:19:35 -0700

    pp

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,26 @@
+const myString = "Learning Javascript";
+const myFirstNumericVariable = 3;
+const myBoolaen = true;
+const myNull = null;
+let myUndefined;
+
+myUndefined = "Learning more java in class"
+
+
+console.log(myString);
+console.log("TYPE", typeof myString);
+console.log(myFirstNumericVariable);
+console.log("TYPE", typeof myFirstNumericVariable);
+console.log(myBoolaen);
+console.log("TYPE", typeof myBoolaen);
+console.log(myNull);
+console.log("TYPE", typeof myNull);
+console.log(myUndefined);
+console.log("TYPE", typeof myUndefined);
+console.log("Add 3 + 25", myFirstNumericVariable + 25);
+console.log("Add undefined", myUndefined + 25);
+
+var oldStyle = "This is the way we used to do it";
+let meChangableStyly = "This allows you to declare a variable that can be changed";
+const constantVariable = "This variable can not be changed except...";
+const quotable = 'he said, "Hello". that is all folks';
